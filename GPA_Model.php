c2bb5e32b2c74d68b98784a12934db65103bc983
aichadiab06-lang <aichadiab06@gmail.com>
2026-04-23 09:57:14 -0700

    Update GPA_Model.php

--- GPA_Model.php
+++ GPA_Model.php
@@ -1,17 +1,37 @@
-
+> Aicha diab:
 <?php
 class GPA_Model {
-    // دالة لحساب المعدل الفصلي
+    /**
+     * دالة لحساب المعدل الفصلي
+     * @param array $grades مصفوفة العلامات
+     * @param array $credits مصفوفة المعاملات
+     * @return float المعدل النهائي مقرباً لرقمين
+     */
     public static function calculate($grades, $credits) {
         $totalPoints = 0;
         $totalCredits = 0;
 
-        foreach ($grades as $index => $grade) {
-            $totalPoints += ($grade * $credits[$index]);
-            $totalCredits += $credits[$index];
+        // التحقق من أن المدخلات مصفوفات وليست فارغة
+        if (empty($grades) || empty($credits)) {
+            return 0;
         }
 
-        return ($totalCredits > 0) ? ($totalPoints / $totalCredits) : 0;
+        foreach ($grades as $index => $grade) {
+            // التأكد من وجود معامل مطابق لكل علامة لتجنب أخطاء الـ Index
+            if (isset($credits[$index])) {
+                $totalPoints += ($grade * $credits[$index]);
+                $totalCredits += $credits[$index];
+            }
+        }
+
+        if ($totalCredits > 0) {
+            $gpa = $totalPoints / $totalCredits;
+            // تقريب المعدل لرقمين بعد الفاصلة (مثلاً 10.55)
+            return round($gpa, 2); 
+        }
+
+        return 0;
     }
 }
 ?>
+
